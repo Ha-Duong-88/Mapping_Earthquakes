3629ba2d05017cf814000dc275f4cd4f682afd45
Ha-Duong-88 <80140082+Ha-Duong-88@users.noreply.github.com>
2021-06-09 06:05:13 -0700

    Added updated static folder with style.css and logic.js files to branch.

---
+++ Mapping_Single_Points/static/js/logic.js
@@ -0,0 +1,34 @@
+// Add console.log to check to see if our code is working.
+console.log("working");
+
+// Create the map object with a center and zoom level.
+// Assign variable map to L.map() and instantiate object with 'mapid' string.
+// The 'mapid' will reference the 'id' tage in our 'div' element in index.html.
+// setview() sets the view of the map with a geographical center.
+let map = L.map('mapid').setView([40.7, -94.5], 4);
+
+// We create the tile layer that will be the background of our map.
+// Tile layer used to load and dispay a tile layer on the map.
+// In this case, it's the Streets tile layer.
+// We assign the tileLayer() method to the variable 'streets'.
+// Using the Mapbox Styles API on Leaflet tileLayer() method here to create tile layer.
+
+let streets = L.tileLayer('https://api.mapbox.com/styles/v1/mapbox/streets-v11/tiles/{z}/{x}/{y}?access_token={accessToken}', {
+attribution: 'Map data © <a href="https://www.openstreetmap.org/">OpenStreetMap</a> contributors, <a href="https://creativecommons.org/licenses/by-sa/2.0/">CC-BY-SA</a>, Imagery (c) <a href="https://www.mapbox.com/">Mapbox</a>',
+    maxZoom: 18,
+    accessToken: API_KEY
+});
+
+// Then we add our 'graymap' tile layer to the map.
+// We call the addTo() function with our map object, map on our graymap object tile layer.
+// The addTo() will add the graymap object tile layer to our let map
+streets.addTo(map);
+
+
+
+
+
+
+
+
+
